c29a9a656778fcdce2b8b8a9c01f8b5db6dc8ad8
Michael Spaulding <mikespaulding3@gmail.com>
2024-06-16 19:38:26 -0700

    bug

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -140,20 +141,18 @@ BOOTSTRAP3 = {
 }
 
 # Heroku settings
-if os.getcwd() == '/app':
+if os.getcwd() == "/app":
     import dj_database_url
-    DATABASES = {
-        'default': dj_database_url.config(default='postgres://localhost')
-    }
+
+    DATABASES = {"default": dj_database_url.config(default="postgres://localhost")}
 
     # Honor the 'X-Forward-Proto' header for request.is_secure().
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARD_PROTO', 'https')
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARD_PROTO", "https")
 
     # Allow all host headers.
-    ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = ["*"]
 
     # Static asset configuration.
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
-    )
+    STATIC_ROOT = "staticfiles"
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
